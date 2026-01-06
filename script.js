38678c14a3477d3e7ad97530a448928ee6ef0b39
Bijeet-D <154560979+Bijeet-D@users.noreply.github.com>
2026-01-05 21:40:55 -0800

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@
     style: "position:fixed;top:0;left:0;width:100%;height:100%;border:none;z-index:9999"
 });
 
-document.body.insertBefore(iframe, scriptTag);
+document.body.appendChild(iframe);
     }
 
   }
